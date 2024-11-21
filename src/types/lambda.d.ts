195f2fea39362f1ee49ed0a7c71da65d501b3f42
eleazar <jai690@hotmail.com>
2024-11-21 15:55:43 -0600

    update extension

--- src/types/lambda.d.ts
+++ src/types/lambda.d.ts
@@ -1,12 +1,15 @@
 import type { Context as LambdaContext } from 'aws-lambda';
 
+export interface CustomAuthorizerContext {
+  user: {
+    userId: string;
+    role: string;
+  };
+}
+
+// Extendemos el tipo Context de forma segura sin referencias circulares
 declare module 'aws-lambda' {
   export interface Context extends LambdaContext {
-    authorizer?: {
-      user: {
-        userId: string;
-        role: string;
-      };
-    };
+    authorizer?: CustomAuthorizerContext;
   }
 }
