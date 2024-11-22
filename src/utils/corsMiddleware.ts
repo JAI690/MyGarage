05d408675665ab65926f007763dc957070b89991
eleazar <jai690@hotmail.com>
2024-11-22 12:50:54 -0600

    HANDLE CORS

--- src/utils/corsMiddleware.ts
+++ src/utils/corsMiddleware.ts
@@ -5,19 +5,6 @@ export const corsMiddleware = (
   handler: (event: APIGatewayProxyEvent, context: CustomContext) => Promise<APIGatewayProxyResult>
 ) => {
   return async (event: APIGatewayProxyEvent, context: CustomContext): Promise<APIGatewayProxyResult> => {
-    // Manejar solicitudes OPTIONS
-    if (event.httpMethod === 'OPTIONS') {
-      return {
-        statusCode: 200,
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'GET,POST,OPTIONS,PUT,DELETE',
-          'Access-Control-Allow-Headers': 'Content-Type,Authorization',
-        },
-        body: '',
-      };
-    }
-
     // Ejecutar el controlador original
     const response = await handler(event, context);
 
