3966f52feb3fdac5fba8243e92290ada245eae41
eleazar <jai690@hotmail.com>
2024-11-22 18:41:23 -0600

    change aws-sdk for dynammodb sdk

--- src/services/delete.ts
+++ src/services/delete.ts
@@ -1,5 +1,5 @@
 import { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from 'aws-lambda';
-import { DynamoDB } from 'aws-sdk';
+import { DynamoDB } from '@@aws-sdk/client-dynamodb/client-dynamodb';
 import { authorize } from '../common/utils/authorize';
 import type { CustomContext } from '../common/types/CustomContext';
 import {corsMiddleware}  from '../common/utils/corsMiddleware';
