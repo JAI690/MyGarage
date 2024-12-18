876d6b64b66a49e52a003a53dcd10167fa640283
eleazar <jai690@hotmail.com>
2024-12-17 21:47:16 -0600

    update delete location

--- src/warehouse/delete.ts
+++ src/warehouse/delete.ts
@@ -6,31 +6,38 @@ import type { CustomContext } from '../common/types/CustomContext';
 
 const dynamoDb = new DynamoDB.DocumentClient();
 
-export const handler = authorize(['Cliente','Admin'])(async (
+export const handler = authorize(['Admin'])(async (
   event: APIGatewayProxyEvent,
   context: CustomContext
 ): Promise<APIGatewayProxyResult> => {
   try {
     const { id } = event.pathParameters || {};
-    
+    if (!id) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ message: 'ID es requerido' }),
+      };
+    }
+
     await dynamoDb
       .delete({
         TableName: 'Warehouse',
-        Key: { ProductID: id },
+        Key: { WarehouseID: id },
       })
       .promise();
 
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: 'Producto eliminado' }),
+      body: JSON.stringify({ message: 'Registro eliminado correctamente' }),
     };
   } catch (error) {
-    console.error('Error al eliminar producto:', error);
+    console.error('Error al eliminar registro:', error);
     return {
       statusCode: 500,
-      body: JSON.stringify({ message: 'Error al eliminar producto' }),
+      body: JSON.stringify({ message: 'Error al eliminar el registro' }),
     };
   }
 });
+
 
 export const endpointHandler = corsMiddleware(handler);
